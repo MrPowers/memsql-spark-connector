a4ed794e9ece4386e25efdddad5d1e632d5d1f23
Adalbert Makarovych <amakarovych-ua@memsql.com>
2020-10-08 09:53:31 +0300

    Changed initial heap size

--- scripts/execute-tests.sh
+++ scripts/execute-tests.sh
@@ -5,7 +5,7 @@ TEST_NUM=${SPLIT:-"0"}
 
 if [ "$TEST_NUM" == '0' ] || [ "$TEST_NUM" == '3' ] || [ "$TEST_NUM" == '6' ]
 then
-  sbt ++2.12.12 test -Dspark.version=3.0.0 -J-Xmx4G -J-Xms4G
+  sbt ++2.12.12 test -Dspark.version=3.0.0 -J-Xmx4G -J-Xms2G
 elif [ "$TEST_NUM" == '1' ] || [ "$TEST_NUM" == '4' ] || [ "$TEST_NUM" == '7' ]
 then
   sbt ++2.11.11 "testOnly -- -l  OnlySpark3" -Dspark.version=2.4.4 -J-Xmx4G -J-Xms4G
